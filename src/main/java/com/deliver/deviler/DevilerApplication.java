775c05fe15077ebda5e4e727fff8dc667085c897
Rhuan <rhuanpablo13@hotmail.com>
2022-03-11 00:36:33 -0300

    Iniciando o projeto

---
+++ src/main/java/com/deliver/deviler/DevilerApplication.java
@@ -0,0 +1,13 @@
+package com.deliver.deviler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevilerApplication.class, args);
+	}
+
+}
