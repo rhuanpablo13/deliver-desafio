696c02349d2bcda3f144326a15d29cbeffd72097
Rhuan <rhuanpablo13@hotmail.com>
2022-03-14 09:31:38 -0300

    ajustes docker

--- src/main/resources/db/migration/mysql/V1__init.sql
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -1,3 +1,9 @@
+-- criando o database de desenvolvimento (que tbm vai servir de teste)
+CREATE DATABASE IF NOT EXISTS deliver_dev; 
+
+-- criando o database de produção
+CREATE DATABASE IF NOT EXISTS deliver_prod;
+
 SET character_set_client = utf8;
 SET character_set_connection = utf8;
 SET character_set_results = utf8;
